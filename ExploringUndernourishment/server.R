10ac0a232fdcc767d48f44ab719a6831f1f2ae15
chrimaho <chrismahoney@hotmail.com>
2020-05-14 00:32:01 +1000

    Add visuals to Stat page.

--- ExploringUndernourishment/server.R
+++ ExploringUndernourishment/server.R
@@ -11,15 +11,83 @@
 #                                                                              #
 #==============================================================================#
 
-# Define the Server ----
+
+#------------------------------------------------------------------------------#
+# Define the Server                                                         ####
+#------------------------------------------------------------------------------#
+
 server <- function(input, output, session) {
-    output$dat_DataDictionary <- renderTable(
-        striped=TRUE,
-        bordered=TRUE,
-        hover=TRUE,
-        spacing="xs",
+    
+    
+    #------------------------------------------------------------------------------#
+    # Info Page                                                                 ####
+    #------------------------------------------------------------------------------#
+    
+    output$tbl_info_DataDictionary <- DT::renderDataTable(
         expr={
             FaoStat_VariableMapping
-            }
+            },
+        options=list(
+            pageLength=100,
+            dom="ft",
+            autoWidth=TRUE,
+            bAutoWidth=TRUE,
+            columnDefs=list(list(ClassName="dt-left", targets="_all"))
         )
+    )
+    
+    
+    #------------------------------------------------------------------------------#
+    # Stat Page                                                                 ####
+    #------------------------------------------------------------------------------#
+    
+    # comment ----
+    output$plt_stat_PrevUndrOverall <- renderPlot(
+        expr={
+            FaoStat_wide %>% 
+                ggplot() +
+                geom_histogram(aes(prevalence_of_undernourishment), bins=30, fill="cornflowerblue", colour="black") +
+                labs(title="Prevalence of Undernourishment",
+                     subtitle="Histogram Plot",
+                     x="Prevalence of Undernourishment",
+                     y="Count",
+                     caption="A neat, right-tailed histogram, with values between 0 and 0.7."
+                )
+        }
+    )
+    
+    # comment ----
+    output$plt_stat_MissingData <- renderPlot(
+        expr={
+            FaoStat_wide %>% 
+                gg_miss_var(show_pct=TRUE) +
+                theme_bw() +
+                theme_update(plot.title = element_text(hjust=0.5)
+                             ,plot.subtitle = element_text(hjust=0.5)
+                )+
+                scale_y_continuous(limits=c(0,100)) +
+                labs(title="Percentage of Missing Values"
+                     ,subtitle="Ordered by percentage missing"
+                     ,y="Percentage Missing"
+                )
+        }
+    )
+    
+    # comment ----
+    output$tbl_stat_DataFrameStats <- DT::renderDataTable(
+        expr={
+            FaoStat_wide %>%
+                get_DataFrameStatistics(signif=2) %>%
+                # select(-c(length, class, type, mode, sum), -contains("null"))
+                select(-c(sum), -contains("null"))
+        },
+        options=list(
+            pageLength=40,
+            dom="ft",
+            scrollX=TRUE,
+            autoWidth=TRUE,
+            columnDefs=list(list(ClassName="dt-left", width="auto", targets="_all"))
+        )
+    )
+    
 }
