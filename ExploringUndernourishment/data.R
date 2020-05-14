569c17a84ae1998f40e23f931a94f8035d131ab2
chrimaho <chrismahoney@hotmail.com>
2020-05-15 06:14:40 +1000

    Rearrange parts, and add `category` to the VariableMapping.

--- ExploringUndernourishment/data.R
+++ ExploringUndernourishment/data.R
@@ -62,36 +62,51 @@ if (!exists("FaoStat_long")) {
         
         #map variable names 
         mutate(variable = case_when(
-            item=="Average dietary energy supply adequacy (percent) (3-year average)" ~ "avg_dietary_adequacy"
-            ,item=="Average value of food production (constant 2004-2006 I$/cap) (3-year average)" ~ "avg_value_of_food_production"
-            ,item=="Share of dietary energy supply derived from cereals, roots and tubers (kcal/cap/day) (3-year average)" ~ "caloric_energy_from_cereals_roots_tubers"
-            ,item=="Average protein supply (g/cap/day) (3-year average)" ~ "avg_protein_supply"
-            ,item=="Average supply of protein of animal origin (g/cap/day) (3-year average)" ~ "avg_supply_of_protein_of_animal_origin"
-            ,item=="Gross domestic product per capita, PPP, dissemination (constant 2011 international $)" ~ "gross_domestic_product_per_capita_ppp"
-            ,item=="Prevalence of undernourishment (percent) (3-year average)" ~ "prevalence_of_undernourishment"
-            ,item=="Number of people undernourished (million) (3-year average)" ~ "num_people_undernourished"
-            ,item=="Prevalence of severe food insecurity in the total population (percent) (3-year average)" ~ "prv_severe_food_insecurity"
-            ,item=="Prevalence of moderate or severe food insecurity in the total population (percent) (3-year average)" ~ "prv_moderate_food_insecurity"
-            ,item=="Number of severely food insecure people (million) (3-year average)" ~ "num_severe_food_insecurity"
-            ,item=="Number of moderately or severely food insecure people (million) (3-year average)" ~ "num_moderate_food_insecurity"
-            ,item=="Cereal import dependency ratio (percent) (3-year average)" ~ "cereal_import_dependency_ratio"
-            ,item=="Percent of arable land equipped for irrigation (percent) (3-year average)" ~ "pct_arable_land"
-            ,item=="Value of food imports in total merchandise exports (percent) (3-year average)" ~ "val_food_imports_as_share_of_merch_exports"
-            ,item=="Political stability and absence of violence/terrorism (index)" ~ "val_political_stability"
-            ,item=="Per capita food production variability (constant 2004-2006 thousand int$ per capita)" ~ "val_food_production_variability"
-            ,item=="Per capita food supply variability (kcal/cap/day)" ~ "val_food_supply_variability"
-            ,item=="Percentage of population using at least basic drinking water services (percent)" ~ "pct_access_to_drinking_water"
-            ,item=="Percentage of population using at least basic sanitation services (percent)" ~ "pct_access_to_sanitation_services"
-            ,item=="Percentage of children under 5 years affected by wasting (percent)" ~ "pct_children_affected_by_wasting"
-            ,item=="Percentage of children under 5 years of age who are stunted (percent)" ~ "pct_children_who_are_stunted"
-            ,item=="Percentage of children under 5 years of age who are overweight (percent)" ~ "pct_children_who_are_overweight"
-            ,item=="Prevalence of obesity in the adult population (18 years and older)" ~ "prv_adults_who_are_overweight"
-            ,item=="Prevalence of anemia among women of reproductive age (15-49 years)" ~ "prv_women_with_anemia"
-            ,item=="Prevalence of exclusive breastfeeding among infants 0-5 months of age" ~ "prv_women_who_can_breastfeed"
-            ,item=="Rail lines density (total route in km per 100 square km of land area)" ~ "val_rail_line_density"
-            ,item=="Percentage of population using safely managed drinking water services (Percent)" ~ "pct_access_to_managed_drinking_water"
-            ,item=="Percentage of population using safely managed sanitation services (Percent)" ~ "pct_access_to_managed_sanitation_services"
-            ,item=="Prevalence of low birthweight (percent)" ~ "prv_low_birthrate"
+            
+            # Target
+            item=="Prevalence of undernourishment (percent) (3-year average)"                                               ~ "prevalence_of_undernourishment"
+            
+            # Agriculture
+            ,item=="Percent of arable land equipped for irrigation (percent) (3-year average)"                              ~ "percentage_of_arable_land"
+            
+            # Economics
+            ,item=="Cereal import dependency ratio (percent) (3-year average)"                                              ~ "cereal_import_dependency_ratio"
+            ,item=="Average value of food production (constant 2004-2006 I$/cap) (3-year average)"                          ~ "avg_value_of_food_production"
+            ,item=="Value of food imports in total merchandise exports (percent) (3-year average)"                          ~ "food_imports_as_share_of_merch_exports"
+            ,item=="Gross domestic product per capita, PPP, dissemination (constant 2011 international $)"                  ~ "gross_domestic_product_per_capita_ppp"
+            
+            # Food Security
+            ,item=="Average supply of protein of animal origin (g/cap/day) (3-year average)"                                ~ "avg_supply_of_protein_of_animal_origin"
+            ,item=="Share of dietary energy supply derived from cereals, roots and tubers (kcal/cap/day) (3-year average)"  ~ "caloric_energy_from_cereals_roots_tubers"
+            ,item=="Per capita food production variability (constant 2004-2006 thousand int$ per capita)"                   ~ "food_production_variability"
+            ,item=="Per capita food supply variability (kcal/cap/day)"                                                      ~ "food_supply_variability"
+            ,item=="Average dietary energy supply adequacy (percent) (3-year average)"                                      ~ "avg_dietary_adequacy"
+            ,item=="Average protein supply (g/cap/day) (3-year average)"                                                    ~ "avg_protein_supply"
+            ,item=="Prevalence of moderate or severe food insecurity in the total population (percent) (3-year average)"    ~ "prevalence_moderate_food_insecurity"
+            ,item=="Number of moderately or severely food insecure people (million) (3-year average)"                       ~ "number_moderate_food_insecurity"
+            ,item=="Prevalence of severe food insecurity in the total population (percent) (3-year average)"                ~ "prevalence_severe_food_insecurity"
+            ,item=="Number of severely food insecure people (million) (3-year average)"                                     ~ "number_severe_food_insecurity"
+            
+            # Health
+            ,item=="Percentage of population using at least basic drinking water services (percent)"                        ~ "access_to_basic_drinking_water"
+            ,item=="Percentage of population using at least basic sanitation services (percent)"                            ~ "access_to_basic_sanitation_services"
+            ,item=="Percentage of population using safely managed drinking water services (Percent)"                        ~ "access_to_improved_drinking_water"
+            ,item=="Percentage of population using safely managed sanitation services (Percent)"                            ~ "access_to_improved_sanitation_services"
+            ,item=="Prevalence of anemia among women of reproductive age (15-49 years)"                                     ~ "prevalence_of_anemia"
+            ,item=="Prevalence of obesity in the adult population (18 years and older)"                                     ~ "prevalence_of_obesity"
+            ,item=="Number of people undernourished (million) (3-year average)"                                             ~ "number_people_undernourished"
+            ,item=="Prevalence of low birthweight (percent)"                                                                ~ "prevalence_of_low_birthrate"
+            ,item=="Percentage of children under 5 years affected by wasting (percent)"                                     ~ "children_affected_by_wasting"
+            ,item=="Percentage of children under 5 years of age who are stunted (percent)"                                  ~ "children_who_are_stunted"
+            ,item=="Percentage of children under 5 years of age who are overweight (percent)"                               ~ "children_who_are_overweight"
+            ,item=="Prevalence of exclusive breastfeeding among infants 0-5 months of age"                                  ~ "prevalence_of_breastfeeding_women"
+            
+            # Infrastructure
+            ,item=="Rail lines density (total route in km per 100 square km of land area)"                                  ~ "rail_line_density"
+            
+            # Politics
+            ,item=="Political stability and absence of violence/terrorism (index)"                                          ~ "political_stability"
+            
         )) %>% 
         
         #fix units
@@ -109,13 +124,7 @@ if (!exists("FaoStat_long")) {
                 rename("description"="item") %>% 
                 distinct() %>% 
                 rbind(c("country", "The country being recorded")) %>% 
-                rbind(c("year", "The year of record")) %>% 
-                mutate(type=case_when(
-                    variable=="country" ~ "identifier"
-                    ,variable=="year" ~ "identifier"
-                    ,variable=="prevalence_of_undernourishment" ~ "target"
-                    ,TRUE ~ "predictor"
-                ))
+                rbind(c("year", "The year of record"))
             return(x)
         }) %>% 
         
@@ -151,7 +160,68 @@ if (!exists("FaoStat_wide")) {
 FaoStat_VariableMapping <- FaoStat_wide %>% 
     names %>% 
     data.frame("variable"=., stringsAsFactors=FALSE) %>% 
-    left_join(y=FaoStat_VariableMapping, by=c("variable"="variable"))
+    left_join(y=FaoStat_VariableMapping, by=c("variable"="variable")) %>% 
+    
+    # Add Category mapping
+    mutate(category=case_when(
+        
+        # Identifier
+        variable %in% c("country", "year") ~ "identifier"
+        
+        # Target
+        ,variable %in% c("prevalence_of_undernourishment") ~ "target"
+        
+        # Agriculture
+        ,variable %in% c("percentage_of_arable_land") ~ "agriculture"
+        
+        # Economics
+        ,variable %in% c("cereal_import_dependency_ratio"
+                        ,"avg_value_of_food_production"
+                        ,"food_imports_as_share_of_merch_exports"
+                        ,"gross_domestic_product_per_capita_ppp"
+                        ) ~ "economics"
+        
+        # Food Security
+        ,variable %in% c("avg_supply_of_protein_of_animal_origin"
+                        ,"caloric_energy_from_cereals_roots_tubers"
+                        ,"food_production_variability"
+                        ,"food_supply_variability"
+                        ,"avg_dietary_adequacy"
+                        ,"avg_protein_supply"
+                        ,"prevalence_moderate_food_insecurity"
+                        ,"number_moderate_food_insecurity"
+                        ,"prevalence_severe_food_insecurity"
+                        ,"number_severe_food_insecurity"
+                        ) ~ "food security"
+        
+        # Health
+        ,variable %in% c("access_to_basic_drinking_water"
+                        ,"access_to_basic_sanitation_services"
+                        ,"access_to_improved_drinking_water"
+                        ,"access_to_improved_sanitation_services"
+                        ,"prevalence_of_anemia"
+                        ,"prevalence_of_obesity"
+                        ,"number_people_undernourished"
+                        ,"prevalence_of_low_birthrate"
+                        ,"children_affected_by_wasting"
+                        ,"children_who_are_stunted"
+                        ,"children_who_are_overweight"
+                        ,"prevalence_of_breastfeeding_women" 
+                        ) ~ "health"
+        
+        # Infrastructure
+        ,variable %in% c("rail_line_density") ~ "infrastructure"
+        
+        # Politics
+        ,variable %in% c("political_stability") ~ "politics"
+        
+    )) %>% 
+    mutate(type=case_when(
+        variable %in% c("country", "year") ~ "identifier"
+        ,variable %in% "prevalence_of_undernourishment" ~ "target"
+        ,TRUE ~ "other"
+    )) %>% 
+    arrange(factor(category, levels=c("identifier", "target", "agriculture", "economics", "food security", "health", "infrastructure", "politics")))
 
 
 #------------------------------------------------------------------------------#
