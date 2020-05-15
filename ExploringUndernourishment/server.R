1b1ba563b1f47b90ef662e7c8f4ed5eb7590500d
chrimaho <chrismahoney@hotmail.com>
2020-05-15 17:57:42 +1000

    Finalise CorrPlot and optimise load time with memory checks.

--- ExploringUndernourishment/server.R
+++ ExploringUndernourishment/server.R
@@ -26,7 +26,7 @@ server <- function(input, output, session) {
     output$tbl_info_DataDictionary <- DT::renderDataTable(
         expr={
             FaoStat_VariableMapping
-            },
+        },
         options=list(
             pageLength=100,
             dom="ft",
@@ -44,72 +44,110 @@ server <- function(input, output, session) {
     # comment ----
     output$plt_stat_PrevUndrOverall <- renderPlot(
         expr={
-            FaoStat_wide %>% 
-                ggplot() +
-                geom_histogram(aes(prevalence_of_undernourishment), bins=30, fill="cornflowerblue", colour="black") +
-                labs(title="Prevalence of Undernourishment",
-                     subtitle="Histogram Plot",
-                     x="Prevalence of Undernourishment",
-                     y="Count",
-                     caption="A neat, right-tailed histogram, with values between 0 and 0.7."
-                )
+            # Optimise for to save future load time.
+            if (exists("plt_stat_PrevUndrOverall")) {
+                plt_stat_PrevUndrOverall
+            } else {
+                # Make
+                plt_stat_PrevUndrOverall <<- FaoStat_wide %>% 
+                    ggplot() +
+                    geom_histogram(aes(prevalence_of_undernourishment),
+                                   bins=30, 
+                                   fill="cornflowerblue", 
+                                   colour="black",
+                                   alpha=0.8,
+                                   size=0.3
+                    ) +
+                    labs(title="Prevalence of Undernourishment",
+                         subtitle="Histogram Plot",
+                         x="Prevalence of Undernourishment",
+                         y="Count",
+                         caption="A neat, right-tailed histogram, with values between 0 and 0.7."
+                    )
+                
+                # Return
+                plt_stat_PrevUndrOverall
+            }
         }
     )
     
     # comment ----
     output$plt_stat_MissingData <- renderPlot(
         expr={
-            FaoStat_wide %>% 
-                miss_var_summary() %>% 
-                left_join(x=., y=FaoStat_VariableMapping %>% select(variable, category), by=c("variable"="variable")) %>% 
-                ggplot(aes(x = stats::reorder(variable, pct_miss))) + 
-                geom_bar(aes(y = pct_miss, colour=category, fill=category)
-                         ,stat = "identity"
-                         ,position = "dodge"
-                         ,width = 0.1
-                ) + 
-                geom_point(aes(y = pct_miss, colour=category)
-                           ,size=3
-                ) + 
-                coord_flip() + 
-                scale_color_brewer(type="qual", palette="Dark2", aesthetics=c("colour", "fill")) +
-                labs(title="Percentage of Missing Values"
-                     ,subtitle="Ordered by percentage missing"
-                     ,y="Percentage Missing"
-                     ,x="Variables"
-                     ,color="Category"
-                     ,fill="Category"
-                )
+            # Optimise for to save future load time.
+            if (exists("plt_stat_MissingData")) {
+                plt_stat_MissingData
+            } else {
+                # Make
+                plt_stat_MissingData <<- FaoStat_wide %>% 
+                    miss_var_summary() %>% 
+                    left_join(x=., y=FaoStat_VariableMapping %>% select(variable, category), by=c("variable"="variable")) %>% 
+                    ggplot(aes(x=stats::reorder(variable, pct_miss))) + 
+                    geom_bar(aes(y=pct_miss, colour=category, fill=category),
+                             stat="identity",
+                             position="dodge",
+                             width=0.1
+                    ) + 
+                    geom_point(aes(y = pct_miss, colour=category)
+                               ,size=3
+                    ) + 
+                    coord_flip() + 
+                    scale_color_brewer(type="qual", 
+                                       palette="Dark2", 
+                                       aesthetics=c("colour", "fill")
+                    ) +
+                    labs(title="Percentage of Missing Values",
+                         subtitle="Ordered by percentage missing",
+                         y="Percentage Missing",
+                         x="Variables",
+                         color="Category",
+                         fill="Category"
+                    )
+                
+                # Return
+                plt_stat_MissingData
+            }
         }
     )
     
     # Heading3 ----
     output$plt_corr_AllVariables <- renderPlot(
         expr={
-            FaoStat_wide %>% 
-                select(-country, -year) %>% 
-                cor(use="pairwise.complete.obs") %>% 
-                ggcorrplot::ggcorrplot(method="square"
-                                       ,type="upper"
-                                       ,show.diag=FALSE
-                                       ,colors = c("blue", "grey", "red")
-                                       ,digits=1
-                                       ,tl.srt=90
-                                       ,lab=TRUE
-                                       ,lab_col="black"
-                                       ,lab_size=3
-                                       ,insig="pch"
-                                       ,pch="?"
-                                       ,pch.cex=3
-                )
+            # Optimise for to save future load time.
+            if (exists("plt_corr_AllVariables")) {
+                plt_corr_AllVariables
+            } else {
+                # Make
+                plt_corr_AllVariables <<- FaoStat_wide %>% 
+                    select(-country, -year) %>% 
+                    extract(ncol(.):1) %>% 
+                    cor(use="pairwise.complete.obs") %>% 
+                    corrplot(method="pie"
+                             ,type="lower"
+                             ,diag=FALSE
+                             ,tl.col="black"
+                    )
+                
+                # Return
+                plt_corr_AllVariables
+            }
         }
     )
     
     # Heading3 ----
     output$plt_hist_FeatureDistributions <- renderPlot(
         expr={
-            FaoStat_wide %>% 
-                plt_grob_MultipleHistograms(c("country", "year"))
+            # Optimise for to save future load time.
+            if (exists("plt_hist_FeatureDistributions")) {
+                plt_hist_FeatureDistributions
+            } else {
+                # Make
+                plt_hist_FeatureDistributions <<- FaoStat_wide %>% 
+                    plt_grob_MultipleHistograms(c("country", "year"))
+                
+                # Return
+                plt_hist_FeatureDistributions
+            }
         }
     )
     
@@ -118,7 +156,6 @@ server <- function(input, output, session) {
         expr={
             FaoStat_wide %>%
                 get_DataFrameStatistics(signif=2) %>%
-                # select(-c(length, class, type, mode, sum), -contains("null"))
                 select(-c(sum), -contains("null"))
         },
         options=list(
