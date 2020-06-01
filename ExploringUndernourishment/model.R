c3c6b99bc0af70690d4d2f7371f3bc3a9d795f9f
chrimaho <chrismahoney@hotmail.com>
2020-06-01 16:19:32 +1000

    Create the /model.R file.

---
+++ ExploringUndernourishment/model.R
@@ -0,0 +1,13 @@
+#==============================================================================#
+#                                                                              #
+#    Title      : Model                                                        #
+#    Purpose    : To model the data                                            #
+#    Notes      : Notes                                                        #
+#    Author     : chrimaho                                                     #
+#    Created    : 01/Jun/2020                                                  #
+#    References : References                                                   #
+#    Sources    : Sources                                                      #
+#    Edited     : 01/Jun/2020 - Initial creation                               #
+#                                                                              #
+#==============================================================================#
+
