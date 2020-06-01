d9d2923372d4a325c5b66bd930ae41f3aa0b795a
chrimaho <chrismahoney@hotmail.com>
2020-06-01 17:12:37 +1000

    Add code to do modeling.

--- ExploringUndernourishment/model.R
+++ ExploringUndernourishment/model.R
@@ -2,7 +2,7 @@
 #                                                                              #
 #    Title      : Model                                                        #
 #    Purpose    : To model the data                                            #
-#    Notes      : Notes                                                        #
+#    Notes      : Currently set up to use the Gradient Boosted Machine (GBM)   #
 #    Author     : chrimaho                                                     #
 #    Created    : 01/Jun/2020                                                  #
 #    References : References                                                   #
@@ -11,3 +11,130 @@
 #                                                                              #
 #==============================================================================#
 
+
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#    Set Up                                                                 ####
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+# . Data                                                                    ####
+#------------------------------------------------------------------------------#
+
+# . . Features ----
+mod_data_Features <- FaoStat_VariableMapping %>% 
+    filter(type=="independant") %>% 
+    select(variable) %>% 
+    pull()
+
+# . . Raw ----
+mod_data_Raw <- FaoStat_wide %>% 
+    select(prevalence_of_undernourishment, all_of(mod_data_Features)) %>% 
+    na.omit()
+
+# . . Predictors ----
+mod_data_Predictors <- mod_data_Raw %>% 
+    select(-prevalence_of_undernourishment)
+
+# . . Response ----
+mod_data_Response <- mod_data_Raw %>% 
+    select(prevalence_of_undernourishment) %>% 
+    pull()
+
+
+#------------------------------------------------------------------------------#
+# . Control                                                                 ####
+#------------------------------------------------------------------------------#
+
+# . . Set ----
+mod_gbm_TrainControl <- trainControl(
+    method="adaptive_cv",
+    number=3,
+    repeats=10,
+    search="grid",
+    verboseIter=FALSE,
+    allowParallel=FALSE
+)
+
+
+#------------------------------------------------------------------------------#
+# . Grid                                                                    ####
+#------------------------------------------------------------------------------#
+
+# . . Set ----
+mod_gbm_TuneGrid <- expand.grid(
+    interaction.depth=c(5,10,30,50,100),
+    n.trees=c(1000,2000,5000,10000),
+    shrinkage=c(0.1,0.01,0.001,0.0001),
+    n.minobsinnode=c(1,2,5,10)
+)
+
+
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#    Train                                                                  ####
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+# . Logic                                                                   ####
+#------------------------------------------------------------------------------#
+
+# The process will check:
+# 1. If the file exists in local directory
+#   1. If Yes, then load and end
+#   2. If No, then proceed to next step
+# 2. If the model exists in the environment
+#   1. If Yes, then end
+#   2. If No, then proceed to next step
+# 3. Run the model
+# The main reason for this three-step process is because the model takes a long time to run.
+# Once deployed to the world, it's better for Shiny to check these locations first, and prevent running the model from scratch.
+
+
+#------------------------------------------------------------------------------#
+# . Run                                                                     ####
+#------------------------------------------------------------------------------#
+
+# . . Check local directory ----
+if (file.exists("./model/mod_gbm_Model.rds")) {
+    
+    # . . Load ----
+    mod_gbm_Model <- read_rds("./model/mod_gbm_Model.rds")
+    
+} else {
+    
+    # . . Check local environment ----
+    if (exists("mod_gbm_Model")) {
+        
+        # . . Re-Load ----
+        mod_gbm_Model <- mod_gbm_Model
+        
+    } else {
+    
+        # . . Train ----
+        tic("GBM Training")
+        mod_gbm_Model <- train(
+            x=mod_data_Predictors,
+            y=mod_data_Response,
+            method="gbm",
+            trControl=mod_gbm_TrainControl,
+            verbose=FALSE,
+            metric="rmse",
+            tuneGrid=mod_gbm_TuneGrid
+        )
+        toc()
+        
+        # . . Save ----
+        write_rds(
+            x=mod_gbm_Model,
+            path="./model/mod_gbm_Model.rds",
+            compress="none"
+        )
+    
+    }
+    
+}
