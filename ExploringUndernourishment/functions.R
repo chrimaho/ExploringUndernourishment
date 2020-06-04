545560f4c87328cfaaf70580361d26b5eb9e9dcc
chrimaho <chrismahoney@hotmail.com>
2020-06-04 13:44:14 +1000

    Add another example to the `str_Format()` function.

--- ExploringUndernourishment/functions.R
+++ ExploringUndernourishment/functions.R
@@ -163,8 +163,9 @@ str_Format <- function(string, ...) {
     #' @param string string. The string to be re-formatted. Note, each of the named arguments must be surrounded in curly brackets.
     #' @param ... variables. A list of variables. Note, these can either be named or not; but they must all be named, or all be blank, because it cannot handle a mixture. Each of these arguments must align to the variables in curly brackets from the `string` argument. These will be combined in to a list.
     #' @return A formatted string
-    #' @example str_Format("Sammy the {animal} {verb} a {noun}.", animal="shark", verb="made", noun="car")
-    #' @example str_Format("Sammy the {} {} a {}.", "shark", "made", "car")
+    #' @example str_Format("Sammy the {animal} {verb} a {noun}.", animal="shark", verb="made", noun="house")
+    #' @example str_Format("Sammy the {} {} a {}.", "shark", "made", "house")
+    #' @example "Sammy the {animal} {verb} a {noun}" %>% str_Format(animal="shark", verb="made", noun="house")
     #' @references https://stackoverflow.com/questions/44763056/is-there-an-r-equivalent-of-pythons-string-format-function#answer-44763659
     #' @author chrimaho
     
